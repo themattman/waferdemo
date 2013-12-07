65f3802827f78ae7d25f66a3bfddd9089f6ede95
Matt Kneiser <mattman@umich.edu>
2013-12-07 14:14:30 -0500

    Working on local+cloud mongo integration into demo app

--- server/app.js
+++ server/app.js
@@ -6,28 +6,35 @@ var express = require('express')
   , secret  = require('./secret.js')
   , http    = require('http')
 
-  , connect_string = "mongodb://"+secret.db.user+":"+secret.db.pass+"@"+secret.db.url+":"+secret.db.port+"/"+secret.db.name
+  , connect_string       = "mongodb://"+secret.db.user+":"+secret.db.pass+"@"+secret.db.url+":"+secret.db.port+"/"+secret.db.name
+  , connect_string_local = "mongodb://"+secret.db.user+":"+secret.db.pass+"@localhost:27017/"+secret.db.name
 
   /** WAFER **/
-  , redis   = require('redis')
-  , client  = redis.createClient()
+  //, redis   = require('redis')
+  //, client  = redis.createClient()
   , mongo   = require('mongodb').MongoClient
   , wafer   = require('waferDB.js/wafer.js').server
 ;
 
+/*
 wafer.init("redis", client);
 client.on('error', function(err){
   console.log(err);
 });
+*/
 
 // Connect to MongoDB
-/*mongo.connect(connect_string, function(msg, db) {
+mongo.connect(connect_string_local, function(msg, db) {
   if(msg == null) {
     console.log("Mongo Connected!".yellow);
+    /*db.collection('waferdb').insert({'_id': 'matttt', 'value': 'proj'}, function(err, result){
+        if(err){throw err;}
+        else console.log(result);
+      });;*/
     wafer.init("mongodb", db.collection('waferdb'));
   } else 
     console.log(msg);
-});*/
+});
 
 // setup here
 config(app);
